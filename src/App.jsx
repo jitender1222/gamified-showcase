1031c62049189e033062ee941ace4a8e7e11fb96
jitender1222 <jeetusinghkanyal@gmail.com>
2025-04-10 11:20:18 +0530

    hero section completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <Hero />
+      <section className="bg-blue-400 h-[500px] mt-10"></section>
+    </>
+  );
+};
+
+export default App;
