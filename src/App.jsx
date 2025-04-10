d01614d184fa94d22ab6dee1671e6d15a93e20ff
jitender1222 <jeetusinghkanyal@gmail.com>
2025-04-10 16:44:32 +0530

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <Hero />
-      <section className="bg-blue-400 h-[500px] mt-10"></section>
+      <About />
     </>
   );
 };
